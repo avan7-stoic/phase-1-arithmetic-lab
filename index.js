b4fb72fd91ff54ac1586571752a23b0c2a655d68
avant muchiri <avant.muchiri@student.moringaschool>
2024-06-29 15:31:03 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.random();
+random = Math.floor(random * 10) + 1;
+console.log(random);
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let numbers = [12, 7, 20, 17, 14];
+let max = Math.max(12, 7, 20, 17, 14);
+console.log(max);
+ // Write your code here
