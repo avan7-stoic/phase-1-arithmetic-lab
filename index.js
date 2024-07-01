3048c8788e75f0fd4e179845306d149d45a93650
avant muchiri <avant.muchiri@student.moringaschool>
2024-07-01 11:02:42 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+ // Write your code here 
 let num1 = 31;
 let num2 = 2;
 let multiply = num1 * num2;
@@ -15,4 +16,3 @@ console.log(mod);
 let numbers = [12, 7, 20, 17, 14];
 let max = Math.max(12, 7, 20, 17, 14);
 console.log(max);
- // Write your code here
